7ab800424ad7ac503875269f0e8c11f51ec88deb
Oleg Beloglazov <r6710997@gmail.com>
2025-01-27 23:12:10 +0700

    1

---
+++ Java_TestTask.java
@@ -0,0 +1,11 @@
+//import java.io;
+
+public class Java_TestTask {
+    public static void main(String[] args) {
+        //System.out.println(args);
+        
+        for (String s: args){
+            System.out.println(s);
+        }
+    }
+}
