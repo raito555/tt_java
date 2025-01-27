66e766c21192322454fdc8aea185b2da2871075f
Oleg Beloglazov <r6710997@gmail.com>
2025-01-27 23:49:29 +0700

    add file reader

--- Java_TestTask.java
+++ Java_TestTask.java
@@ -1,11 +1,21 @@
-//import java.io;
+import java.io.*;
 
 public class Java_TestTask {
     public static void main(String[] args) {
-        //System.out.println(args);
-        
+
+        // для каждого из аргумента параметра запуска выполняем
         for (String s: args){
-            System.out.println(s);
+            try(FileReader reader = new FileReader(s)){
+                // посимвольное читаем и выводим в консоль
+                int symbol; 
+                while((symbol = reader.read()) != -1){
+                    System.out.print((char)symbol);
+                }
+            }
+            catch(IOException ex){
+                System.out.println(ex.getMessage());
+            }
+            //System.out.println(s);
         }
     }
 }
