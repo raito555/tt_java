5f0592ef3cb4a109e2e6aeadf24b8cf636c36f6c
Oleg Beloglazov <r6710997@gmail.com>
2025-02-01 19:06:01 +0700

    изменил метод проверки типа данных

--- Java_TestTask.java
+++ Java_TestTask.java
@@ -1,31 +1,30 @@
 import java.io.*;
+//import test.java;
+import java.util.Scanner;
 
 public class Java_TestTask {
+
     public static void main(String[] args) {
 
-        // для каждого из аргумента параметра запуска выполняем
         for (String s: args){
-            try(FileReader reader = new FileReader(s)){
-                // посимвольное читаем и выводим в консоль
-                int symbol; 
-                String textFile = "";
-                while((symbol = reader.read()) != -1){
-                    //System.out.print((char)symbol);
-                    textFile += (char)symbol;
-                }
-                
-                // Разделение на строки
-                String[] linesOfTextFile = textFile.split("\\n");
 
-                for(String line : linesOfTextFile){
-                    System.out.println(line);
+            try(FileReader reader = new FileReader(s)){
+                
+                Scanner scanner = new Scanner(reader);
+
+                if (scanner.hasNextInt()){
+                    String line = scanner.nextLine();
+                    System.out.println("Integer - " + line);
+                } else {
+                    System.out.println("Не интеджер");
                 }
+
+                scanner.close();
             }
+            
             catch(IOException ex){
                 System.out.println(ex.getMessage());
             }
-
-
         }
     }
 }
